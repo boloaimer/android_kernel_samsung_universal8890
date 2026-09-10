fd90047c4d566a4e04f2119c0233caf74c0fa9a1
boloaimer <q2591187844@gmail.com>
2026-09-10 23:27:55 +0800

    Revert "kernel: initialize and free cpufreq stats properly"
    This reverts commit 96607f3d8cf382b31483074a5f1ed77eb9946e60.

--- kernel/fork.c
+++ kernel/fork.c
@@ -74,7 +74,6 @@
 #include <linux/uprobes.h>
 #include <linux/aio.h>
 #include <linux/compiler.h>
-#include <linux/cpufreq.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -218,9 +217,6 @@ static void account_kernel_stack(struct thread_info *ti, int account)
 
 void free_task(struct task_struct *tsk)
 {
-#ifdef CONFIG_CPU_FREQ_STAT
-	cpufreq_task_stats_free(tsk);
-#endif
 	account_kernel_stack(tsk->stack, -1);
 	arch_release_thread_info(tsk->stack);
 	free_thread_info(tsk->stack);
